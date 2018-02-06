681d82e1c31de37d793186d8a9fb91d1022eefaf
Markus Konrad <markus.konrad@wzb.eu>
2018-02-06 17:46:44 +0100

    tm_eval with parameters

--- tm_eval.py
+++ tm_eval.py
@@ -13,23 +13,37 @@ from tmtoolkit.lda_utils.common import results_by_parameter
 from tmtoolkit.lda_utils.visualize import plot_eval_results
 
 
-DATA_PICKLE_DTM = 'data/speeches_tokens_nosalut.pickle'
+DATA_PICKLE_DTM = 'data/speeches_tokens_%d.pickle'
+
 
 logging.basicConfig(level=logging.INFO)
 tmtoolkit_log = logging.getLogger('tmtoolkit')
 tmtoolkit_log.setLevel(logging.INFO)
 tmtoolkit_log.propagate = True
 
-print('loading DTM...')
-doc_labels, vocab, dtm = unpickle_file(DATA_PICKLE_DTM)
+
+if len(sys.argv) != 5:
+    print('call script as: %s <tokens preprocessing pipeline> <eta> <alpha factor> <num. iterations>' % sys.argv[0])
+    print('<tokens preprocessing pipeline> must be 0, 1 or 2')
+    exit(1)
+
+preproc_mode = int(sys.argv[1])
+assert 0 <= preproc_mode <= 2
+eta = float(sys.argv[2])
+assert 0 < eta < 1
+alpha_mod = float(sys.argv[3])
+assert alpha_mod > 0
+n_iter = int(sys.argv[4])
+assert n_iter > 0
+
+dtm_pickle = DATA_PICKLE_DTM % preproc_mode
+print('loading DTM from file `%s`...' % dtm_pickle)
+doc_labels, vocab, dtm = unpickle_file(dtm_pickle)
 assert len(doc_labels) == dtm.shape[0]
 assert len(vocab) == dtm.shape[1]
 print('loaded DTM with %d documents, %d vocab size, %d tokens' % (len(doc_labels), len(vocab), dtm.sum()))
 
 print('evaluating topic models...')
-eta = float(sys.argv[1]) if len(sys.argv) >= 2 else 0.01  # lda default: 0.01
-alpha_mod = float(sys.argv[2]) if len(sys.argv) >= 3 else 1.0
-n_iter = int(sys.argv[3]) if len(sys.argv) >= 4 else 2000  # lda default: 2000
 constant_params = dict(n_iter=n_iter,
 #                       random_state=1,
                        eta=eta)
@@ -44,7 +58,7 @@ pprint(varying_params)
 
 eval_results = tm_lda.evaluate_topic_models(dtm, varying_params, constant_params)
 
-pickle_file_eval_res = 'data/tm_eval_results2_eta_%f.pickle' % eta
+pickle_file_eval_res = 'data/tm_eval_results_tok%d_eta_%.2f_alphamod_%.2f.pickle' % (preproc_mode, eta, alpha_mod)
 print('saving results to file `%s`' % pickle_file_eval_res)
 pickle_data(eval_results, pickle_file_eval_res)
 
@@ -53,8 +67,8 @@ eval_results_by_n_topics = results_by_parameter(eval_results, 'n_topics')
 fig, ax = plt.subplots()
 plot_eval_results(fig, ax, eval_results_by_n_topics,
                   xaxislabel='num. topics k', yaxislabel='normalized metric result',
-                  title='Evaluation results for alpha=%d/k, beta=%.2f' % (alpha_mod, eta))
-plot_file_eval_res = 'fig/tm_eval_results2_eta_%f.png' % eta
+                  title='Evaluation results for alpha=%.2f/k, beta=%.2f' % (alpha_mod, eta))
+plot_file_eval_res = 'fig/tm_eval_results_tok%d_eta_%.2f_alphamod_%.2f.png' % (preproc_mode, eta, alpha_mod)
 print('saving plot to file `%s`' % plot_file_eval_res)
 plt.tight_layout()
 plt.savefig(plot_file_eval_res)
