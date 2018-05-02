f6b6b121fa616819dd4b4fff5fd7e1afd17d9e00
Markus Konrad <markus.konrad@wzb.eu>
2018-05-02 15:04:17 +0200

    top topics per party

--- example_analyses.py
+++ example_analyses.py
@@ -6,12 +6,24 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from tmtoolkit.utils import unpickle_file
+from tmtoolkit.topicmod.model_stats import get_most_relevant_words_for_topic, get_topic_word_relevance, \
+    get_doc_lengths, exclude_topics
 
 pd.set_option('display.width', 180)
 
 #%%
 
 doc_labels, vocab, dtm, model = unpickle_file('data/model2.pickle')
+
+n_docs, n_topics = model.doc_topic_.shape
+_, n_vocab = model.topic_word_.shape
+
+assert n_docs == len(doc_labels) == dtm.shape[0]
+assert n_topics == model.topic_word_.shape[0]
+assert n_vocab == len(vocab) == dtm.shape[1]
+
+print('loaded model with %d documents, vocab size %d, %d tokens and %d topics'
+      % (n_docs, n_vocab, dtm.sum(), n_topics))
 
 speeches_merged = unpickle_file('data/speeches_merged.pickle')
 
@@ -28,6 +40,15 @@ for c1, c2 in zip(u'äöüé ', u'aoue-'):
 #mdb['speaker_fp'] = mdb.profile_url.str.extract(r'/([a-z0-9-]+)$')
 
 assert sum(mdb['speaker_fp'].isna()) == 0
+
+#%%
+exclude_topic_indices = np.array([2, 19, 72, 9, 114, 18, 87, 30, 16, 6])
+print('excluding %d topics: %s' % (len(exclude_topic_indices), exclude_topic_indices+1))
+theta, phi = exclude_topics(exclude_topic_indices, model.doc_topic_, model.topic_word_)
+
+n_topics = theta.shape[1]
+assert phi.shape[0] == n_topics
+del model
 
 #%%
 pttrn_doc_label = re.compile(u'(\d+)_sess(\d+)_top([\d-]+)_spk_([a-zß0-9-]+)_seq(\d+)', re.UNICODE)
@@ -71,30 +92,46 @@ print('no MDB data found for %d speeches from %d overall speeches' % (n_no_mdb_d
 
 
 #%%
-avg_topics_per_party = {}
+stats_per_party = {}
 for party, grp in doc_meta.groupby('party'):
     party_speeches_ind = np.where(np.isin(doc_labels, grp.doc_label))[0]
 
-    party_doc_topic = model.doc_topic_[party_speeches_ind, :]
-    avg_topics_per_party[party] = np.sum(party_doc_topic, axis=0) / len(party_doc_topic)
-    assert np.isclose(avg_topics_per_party[party].sum(), 1)
+    party_doc_topic = theta[party_speeches_ind, :]
+    avg_party_theta = np.sum(party_doc_topic, axis=0) / len(party_doc_topic)
+    assert np.isclose(avg_party_theta.sum(), 1)
+
+    sd_party_theta = np.std(party_doc_topic, axis=0)
+    assert avg_party_theta.shape == (n_topics,)
+    assert sd_party_theta.shape == (n_topics,)
+    stats_per_party[party] = (avg_party_theta, len(grp), sd_party_theta)
 
 #%%
-n_parties = len(avg_topics_per_party)
-n_top = 5
-fig, axes = plt.subplots(n_parties, 1, sharex=True, figsize=(8, 2 * n_parties))
+n_parties = len(stats_per_party)
+n_top_topics = 5
+n_top_words = 8
+fig, axes = plt.subplots(n_parties, 1, sharex=True, figsize=(8, 2 * n_parties), constrained_layout=True)
 
-for i, (party, ax) in enumerate(zip(sorted(avg_topics_per_party.keys()), axes)):
-    #print(party)
-    theta = avg_topics_per_party[party]
-    top_topics_ind = np.argsort(theta)[::-1][:n_top]
-    ypos = np.arange(n_top)[::-1]
-    ax.barh(ypos, theta[top_topics_ind], color='lightgray')
+fig.suptitle(u'Top %d average topic proportions per party' % n_top_topics, fontsize='large')
+fig.subplots_adjust(top=0.925)
+
+topic_word_rel_mat = get_topic_word_relevance(phi, theta, get_doc_lengths(dtm), lambda_=0.6)
+
+for i, (party, ax) in enumerate(zip(sorted(stats_per_party.keys()), axes)):
+    theta_party, n_speeches_party, _ = stats_per_party[party]
+    top_topics_ind = np.argsort(theta_party)[::-1][:n_top_topics]
+    ypos = np.arange(n_top_topics)[::-1]
+
+    ax.barh(ypos, theta_party[top_topics_ind], color='lightgray')
     ax.set_yticks(ypos)
     ax.set_yticklabels([u'topic %d' % (t+1) for t in top_topics_ind])
-    ax.set_title(party.decode('utf-8'))
+    ax.set_title(u'%s (N=%d)' % (party.decode('utf-8'), n_speeches_party), fontsize='medium')
+
+    for y, t in zip(ypos, top_topics_ind):
+        most_rel_words = get_most_relevant_words_for_topic(vocab, topic_word_rel_mat, t, n_top_words)
+        ax.text(0.002, y-0.15, ', '.join(most_rel_words), fontsize='xx-small')
 
     if i == n_parties-1:
-        ax.set_xlabel(u'proportion')
+        ax.set_xlabel(u'average proportion')
 
+plt.savefig('fig/top_topics_per_party.png')
 plt.show()
